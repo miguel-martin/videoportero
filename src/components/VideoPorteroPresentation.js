888b17588156744d357d3a3ea3fbf73fd8f4a57c
Miguel Martín <miguel@MacBook-Pro-de-Miguel.local>
2020-10-28 21:41:25 +0100

    Added some more form fields and filled up each company rating

--- src/components/VideoPorteroPresentation.js
+++ src/components/VideoPorteroPresentation.js
@@ -1,12 +1,24 @@
 /**
- * presentational component - render data
+ * Presentational component. Renders the form.
  */
 export const VideoPorteroPresentation = (props) => {
 
     const handleSubmission = (e) => {
-        const selectedProvider = document.querySelector('input[name="providers-radio"]:checked').value;
-        // console.log(`You selected ${selectedProvider}`)
-        const data = { provider: selectedProvider.replace('choice-','')}
+        const provider = document.querySelector('input[name="providers-radio"]:checked')
+                                    ? document.querySelector('input[name="providers-radio"]:checked').value.replace('choice-','')
+                                    : null
+
+        const hasHandFree = document.querySelector('input[name="freeHands"]').checked
+        const keyNumber = document.querySelector('input[name="keys"]').value
+        //console.log(`You selected ${selectedProvider}`)
+        //console.log(hasHandFree)
+        //console.log(keyNumber)
+        const data = { 
+            hasHandFree,
+            keyNumber,
+            provider
+        }
+
         props.dataHandler(data)
     }
 
@@ -26,11 +38,19 @@ export const VideoPorteroPresentation = (props) => {
                 <fieldset className="fieldset">
                     {Providers}
                 </fieldset>
+                <fieldset className="fieldset">
+                    <input name="freeHands" type="checkbox" onChange={ handleSubmission } />
+                    <label htmlFor="freeHands">Manos libres</label>
+                </fieldset>
+                <fieldset className="fieldset">
+                    <input name="keys" type="text" onChange={ handleSubmission } />
+                    <label htmlFor="keys">Llaves de proximidad</label>
+                </fieldset>
             </form>
             <span className="message">
                 { props.total === 0
                         ? 'Seleccione sus preferencias para calcular importe'
-                        : `Coste de su elección ${props.total} euros por vecino.`
+                        : `Coste de su elección: ${props.total} euros por vecino.`
                 }
             </span>
         </div>
--- src/containers/VideoPortero.js
+++ src/containers/VideoPortero.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
 import { VideoPorteroPresentation } from '../components/VideoPorteroPresentation'
 
-const PROVIDERS = ['Dates SL', 'Abatronic', 'Emitek']
+const VECINOS = 18
+
+const PROVIDERS_FEES = {
+    DatesSL: {
+            basePrice: 2835,
+            proximity: 110,
+            key: 4,
+            handsFree: 15
+    },
+    Abatronic: {
+            basePrice: 2477.5,
+            proximity: 0,
+            key: 5,
+            handsFree: 0,
+    },
+    Emitek: {
+            basePrice: 2830.5,
+            proximity: 0,
+            key: 4.1322,
+            handsFree: 28.93,
+    },
+}
+
+const PROVIDERS = Object.keys(PROVIDERS_FEES)
+
+const iva = (n) => (n*1.21) 
+const reparto = (n) => (n/VECINOS)
 
 export class VideoPorteroContainer extends Component {
     state = {
@@ -11,23 +37,21 @@ export class VideoPorteroContainer extends Component {
         total: 0,
     }
 
-    _iva = (n) => (n*1.21) 
-
 
     _updateState = (data) => {
         const provider = data.provider
         console.log("Selected provider: ", PROVIDERS[provider])
         this.setState( { provider: PROVIDERS[provider] })
-        
+
         switch(parseInt(provider)){
             case 0: // Dates SL
-                this.setState({ total: this._iva(2835) })
+                this.setState({ total: reparto(iva(2835)) })
                 break;
             case 1: // Abatronic
-                this.setState({ total: this._iva(2477.5) })
+                this.setState({ total: reparto(iva(2477.5)) })
                 break
             case 2: // Emitek
-                this.setState({ total: this._iva(2830) })
+                this.setState({ total: reparto(iva(2830)) })
                 break
             default:
                 this.setState({ total: 0 })
