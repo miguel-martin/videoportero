c10cfc57a553a7b7aecc359a5aad1f2342a2c8ca
Miguel Martín <miguel@MacBook-Pro-de-Miguel.local>
2020-10-29 11:51:07 +0100

    Ready to deploy...

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders providers', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Manos libres/i);
   expect(linkElement).toBeInTheDocument();
 });
